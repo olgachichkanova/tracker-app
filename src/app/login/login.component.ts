97577aabf2bc64f74f1ae865a9f35f3e67f47456
Olga Andriychenko <olga@glasnostic.com>
2022-08-18 19:05:10 +0800

    added login page and route guards

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/auth.service';
+import { User } from '../shared/interface';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+  submitted: boolean = false;
+
+  constructor(
+    public auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required, 
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    })
   }
 
+  submit() {
+    if(this.form.invalid) {
+      return;
+    }
+
+    this.submitted = true;
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/dashboard'])
+      this.submitted = false;
+    }), () => {
+      this.submitted = false;
+    }
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  apiKey: 'AIzaSyCsTqexfEkN-_gyMKmrZ-_jvgJ3OJ7ud0U',
   production: true
 };
