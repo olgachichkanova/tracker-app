bba6ae3cc4f36d606e9cd659f57e7942284990f7
Olga Andriychenko <olga@glasnostic.com>
2022-09-05 20:46:11 +0800

    added add and delete task functions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import { Environment } from "./interface";
 
 export const environment: Environment = {
   apiKey: 'AIzaSyCsTqexfEkN-_gyMKmrZ-_jvgJ3OJ7ud0U',
-  production: true
+  production: true,
+  fbDbUrl: 'https://tracker-app-522c0-default-rtdb.firebaseio.com'
 };
